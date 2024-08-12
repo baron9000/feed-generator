3d878c8f7d974067dd99644a59409d0a42749e93
Baron von Paddington <shawn.blaylock@gmail.com>
2024-08-12 14:48:42 -0400

    algo update

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -1,7 +1,7 @@
 import { InvalidRequestError } from '@atproto/xrpc-server'
 import { Server } from '../lexicon'
 import { AppContext } from '../config'
-import algos from '../algos'
+import { algos } from '../algos'
 import { validateAuth } from '../auth'
 import { AtUri } from '@atproto/syntax'
 
